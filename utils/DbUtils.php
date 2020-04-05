60d2c406695826aec43fbd0e3615b368856f29af
ayoub el abassi <ayoubelabassi0@gmail.com>
2020-04-05 15:34:35 +0100

    comments

--- utils/DbUtils.php
+++ utils/DbUtils.php
@@ -15,6 +15,12 @@ class DbUtils
         return $con;
     }
 
+    /**
+     * Méthode pour inserer, modifier et supprimer les données à partir de base de données
+     * @param $query
+     * @param $params
+     * @return bool
+     */
     public function executeUpdate($query, $params)
     {
         $con = self::getConnection();
@@ -23,6 +29,11 @@ class DbUtils
         return true;
     }
 
+    /**
+     * Fonction pour récupérer les données à partir de la base de données utilisant une réquet SQL
+     * @param $query
+     * @return false|PDOStatement
+     */
     public function executeQuery($query)
     {
         $pdo = self::getConnection();
